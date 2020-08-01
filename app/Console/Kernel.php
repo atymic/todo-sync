aa1a4102fcba6546f3f2c2177a6e571f3b312b3e
atymic <atymicq@gmail.com>
2020-08-01 11:34:44 +1000

    fix: disable delete reminders

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(QueueSyncJobs::class)->everyMinute();
-        $schedule->command(RemovedPassedGoogleReminders::class)->everyMinute();
+//        $schedule->command(RemovedPassedGoogleReminders::class)->everyMinute();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
